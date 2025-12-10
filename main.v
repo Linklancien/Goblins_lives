f45f69ea382e1a57e19e9223588cabf2e86bb144
Linklancien <123779978+Linklancien@users.noreply.github.com>
2025-12-11 00:10:10 +0100

    Implementing gg.Context

--- main.v
+++ main.v
@@ -5,8 +5,27 @@ import linklancien.gg_plot
 import gg
 
 struct App {
+mut:
+	ctx &gg.Context = unsafe { nil }
 }
 
 fn main() {
 	mut app := &App{}
+	app.ctx = gg.new_context(
+		fullscreen:    false
+		width:         600
+		height:        600
+		create_window: true
+		window_title:  '-Goblins Lives-'
+		bg_color:      gg.gray
+		user_data:     app
+		frame_fn:     on_frame
+		sample_count: 4
+	)
+
+	app.ctx.run()
+}
+
+fn on_frame(mut app App){
+
 }
